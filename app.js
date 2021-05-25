001b8001184702769ca0c397e510ad6845957574
Sahil Nenwani <sk9941274@gmail.com>
2021-05-25 13:10:45 +0500

    btn.addEventListener(click, () => {

--- app.js
+++ app.js
@@ -1,9 +1,12 @@
-const colors = ["black", "red", "purple", "Orange"];
+const colors = ["black", "red", "purple"] ;
+const hex = [0, 1, 2, 3, 4, 5, 6, 7, 8, 9, "A", "B", "C", "D", "E", "F"];
+
 
 const btn = document.getElementById("btn");
 const btn1 = document.getElementById("btn1");
 const btn2 = document.getElementById("btn2");
 const btn3 = document.getElementById("btn3");
+const nav=document.querySelector('.navbar');
 
 const color = document.querySelector('.color');
 
@@ -11,19 +14,23 @@ btn.addEventListener("click", () => {
     const selectcolor = 0;
     document.body.style.backgroundColor = colors[selectcolor];
     color.textContent = colors[selectcolor];
+    nav.style.backgroundColor=colors[selectcolor];
 });
 
 btn1.addEventListener("click", () => {
     const selectcolor = 1;
     document.body.style.backgroundColor = colors[selectcolor];
     color.textContent = colors[selectcolor];
+    nav.style.backgroundColor=colors[selectcolor];
 });
 
 btn2.addEventListener("click", () => {
     const selectcolor = 2;
     document.body.style.backgroundColor = colors[selectcolor];
     color.textContent = colors[selectcolor];
+    nav.style.backgroundColor=colors[selectcolor];
 });
+
 btn3.addEventListener("click", () => {
     const selectcolor = 3;
     document.body.style.backgroundColor = colors[selectcolor];
@@ -31,3 +38,19 @@ btn3.addEventListener("click", () => {
 });
 
 
+btn3.addEventListener("click", () => {
+    let hexvalue="#";
+    for (let index = 0; index < 6; index++) {
+        hexvalue += hex[randomnumber()];
+        
+    }
+    nav.style.backgroundColor=hexvalue;
+    document.body.style.backgroundColor=hexvalue;
+    color.textContent=hexvalue;
+    
+    })
+    
+    
+    const randomnumber= () => (
+       Math.floor(Math.random()* hex.length)
+    );
